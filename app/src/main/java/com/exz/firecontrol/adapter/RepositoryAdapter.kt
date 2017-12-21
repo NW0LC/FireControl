2b02ef83b60c38f7d27657c41cf1b13f1f808b22
史忠文 <704796146@qq.com>
2017-12-21 16:50:35 +0800

    接口部分。

--- app/src/main/java/com/exz/firecontrol/adapter/RepositoryAdapter.kt
+++ app/src/main/java/com/exz/firecontrol/adapter/RepositoryAdapter.kt
@@ -3,15 +3,16 @@ package com.exz.firecontrol.adapter
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.exz.firecontrol.R
-import com.exz.firecontrol.bean.RepositoryBean
+import com.exz.firecontrol.bean.KnowledgeCategoryListBean
+import kotlinx.android.synthetic.main.item_repository.view.*
 
 /**
  * Created by pc on 2017/12/20.
  */
 
-class RepositoryAdapter : BaseQuickAdapter<RepositoryBean, BaseViewHolder>(R.layout.item_repository, null) {
+class RepositoryAdapter<T: KnowledgeCategoryListBean.CategoryListBean> : BaseQuickAdapter<T, BaseViewHolder>(R.layout.item_repository, ArrayList<T>()) {
 
-    override fun convert(helper: BaseViewHolder, item: RepositoryBean) {
-        var v = helper.itemView
+    override fun convert(helper: BaseViewHolder, item: T) {
+       helper.itemView.tv_name.text=item.categoryName
     }
 }
